9a9307a364786c0ca567c3f3fe50d24388dc8d36
mattbrana <150088003+mattbrana@users.noreply.github.com>
2024-11-30 14:46:18 -0500

    Initial

---
+++ Shared/Pixel.cs
@@ -0,0 +1,10 @@
+﻿namespace DRAWINGwebsite.Shared
+{
+    public class Pixel
+    {
+        public byte red { get; set; }
+        public byte green { get; set; }
+        public byte blue { get; set; }
+
+    }
+}
